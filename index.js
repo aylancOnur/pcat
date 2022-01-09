460f641c43291e074a93d2c3a351c8222ae88788
Fr3shBrain <aylanc.onur@gmail.com>
2022-01-09 22:35:33 +0300

    Static files added

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
