662bfe33c22fb5b7704205ae0ffe867e31eb8f04
Fr3shBrain <aylanc.onur@gmail.com>
2022-01-11 19:05:13 +0300

    request body

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
+
+// MIDLEWARES
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true })); // url deki datayı okumamızı sağlıyor.
+app.use(express.json()); // datayı json a çevirmek için
 
 const port = 5000;
 
@@ -19,6 +23,11 @@ app.get("/about", (req, res) => {
 
 app.get("/add", (req, res) => {
   res.render("add");
+});
+
+app.post("/photos", (req, res) => {
+  console.log(req.body);
+  res.redirect("/ ");
 });
 
 app.listen(port, () => {
