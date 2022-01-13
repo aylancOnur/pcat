e4b88ebf797a524d7f32dea5f61b60ec124c2a4b
Fr3shBrain <aylanc.onur@gmail.com>
2022-01-13 21:59:57 +0300

    upload image

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
 const Photo = require("./models/Photo");
 
 const app = express();
@@ -14,6 +16,7 @@ mongoose.connect("mongodb://localhost/pcat", {
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true })); // url deki datayı okumamızı sağlıyor.
 app.use(express.json()); // datayı json a çevirmek için
+app.use(fileUpload());
 
 const port = 5000;
 
@@ -22,7 +25,7 @@ app.set("view engine", "ejs");
 
 //ROUTES
 app.get("/", async (req, res) => {
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort('-dateCreated');
   res.render("index", {
     photos,
   });
@@ -42,9 +45,22 @@ app.get("/add", (req, res) => {
 });
 
 app.post("/photos", async (req, res) => {
-  // async - await yapısı kullanacğız.
-  await Photo.create(req.body); // body bilgisini Photo modeli sayesinde veritabanında dökümana dönüştürüyoruz.
-  res.redirect("/");
+  const uploadDir = "public/uploads";
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + "/public/uploads/" + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: "/uploads/" + uploadeImage.name,
+    }); // body bilgisini Photo modeli sayesinde veritabanında dökümana dönüştürüyoruz.
+    res.redirect("/");
+  });
 });
 
 app.listen(port, () => {
